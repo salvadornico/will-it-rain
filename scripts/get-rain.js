aa611345a0848f07a484f0f0dbd0b1470c736d30
Nico Salvador <nicosalvador@creatinginfo.com>
2018-08-07 17:28:37 +0800

    Testing JS version

---
+++ scripts/get-rain.js
@@ -0,0 +1,18 @@
+import fetch from "node-fetch"
+
+const getJson = url => fetch(url).then(data => data.json())
+// to be replaced during build
+const darkSkyKey = "DARKSKY_TOKEN"
+
+export const handler = (event, context, callback) => {
+	const darkSkyUrl = `https://api.darksky.net/forecast/${darkSkyKey}/14.6102,121.0736`
+
+	getJson(darkSkyUrl).then(data => {
+		const response = {
+			statusCode: 200,
+			body: JSON.stringify(data)
+		}
+
+		callback(undefined, response)
+	})
+}
